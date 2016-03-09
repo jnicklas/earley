9cda241df0edc8b147726bb30422fd4f366ceb9b
Jonas Nicklas <jonas.nicklas@gmail.com>
2016-03-09 01:51:16 +0100

    Revert "Revert funky syntax for terminals/non-terminals"
    This reverts commit 6022b598b5148abd26ac72c12be627e74e7b12d8.

--- tests/rules.rs
+++ tests/rules.rs
@@ -5,12 +5,12 @@ use earley::*;
 
 fn grammar() -> Grammar<u32> {
     let productions: Vec<Box<Production<u32>>> = vec![
-        earley_production!("A" => [Terminal("a")] (_result) -> u32; { 1 }),
-        earley_production!("A" => [NonTerminal("B")] (_result) -> u32; { 1 }),
-        earley_production!("C" => [Terminal("c")] (_result) -> u32; { 1 }),
-        earley_production!("B" => [NonTerminal("C")] (_result) -> u32; { 1 }),
-        earley_production!("C" => [Terminal("d")] (_result) -> u32; { 1 }),
-        earley_production!("C" => [Terminal("e")] (_result) -> u32; { 1 }),
+        earley_production!("A" => [["a"]] (_result) -> u32; { 1 }),
+        earley_production!("A" => [{"B"}] (_result) -> u32; { 1 }),
+        earley_production!("C" => [["c"]] (_result) -> u32; { 1 }),
+        earley_production!("B" => [{"C"}] (_result) -> u32; { 1 }),
+        earley_production!("C" => [["d"]] (_result) -> u32; { 1 }),
+        earley_production!("C" => [["e"]] (_result) -> u32; { 1 }),
     ];
 
     Grammar::new(productions)
